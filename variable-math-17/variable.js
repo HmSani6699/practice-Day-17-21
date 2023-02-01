2f17e46fdfe401462637df02406ecf328f33437c
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-01 08:42:24 +0600

    ad to variable naming convention

--- variable-math-17/variable.js
+++ variable-math-17/variable.js
@@ -29,5 +29,22 @@ var atik;
 //null
 
 var sani = null;
+// console.log(typeof sani);
 
-console.log(typeof sani)
+/* 
+//------- allow is variable name ----//
+1. var firstName='sani';
+2. var ami_vallo=true;
+3. var PakundaMdrasa='vallo;
+
+*/
+
+
+
+/*
+//------- not allow is variable name ----//
+1. var 2name='sani';
+2. var +number=30;
+3. var first last='md manik';
+4. var javascript='valo';
+*/
