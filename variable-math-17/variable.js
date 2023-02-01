c67d84c0fe6ca35cbbd35e5468b2b69760813fdf
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-01 08:31:49 +0600

    ad to new file

---
+++ variable-math-17/variable.js
@@ -0,0 +1,33 @@
+/* 
+---var---
+*/
+
+//variable declaration
+var name;
+//variable assign
+variable = 30
+//variable expration
+var num = 10 + 20;
+// console.log(variable);
+
+
+//variable  type
+
+//numeric
+var number = 200;
+
+//String
+var name = 'Sadiq';
+
+//Boolean
+var amiVallo = true;
+var tomiVallo = false;
+
+//undefined
+var atik;
+
+//null
+
+var sani = null;
+
+console.log(typeof sani)
