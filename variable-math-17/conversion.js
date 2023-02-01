b12199e311318207dbf8b403be2d61735b8bdb7d
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-01 09:28:00 +0600

    ad to dacimal number part

--- variable-math-17/conversion.js
+++ variable-math-17/conversion.js
@@ -6,4 +6,12 @@ var convert = parseInt(number);
 //parseFloat
 var num = '20.56';
 var convert_1 = parseFloat(num)
-console.log(convert_1)
+
+
+//toFixed
+var num = 0.1;
+var num_1 = 0.2;
+var total = num + num_1;
+var fixed = total.toFixed(2);
+var float = parseFloat(fixed);
+console.log(float)
